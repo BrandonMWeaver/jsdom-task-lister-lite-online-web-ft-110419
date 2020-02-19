a9b5c634ade9a13f533c43f0fea39fefd0093bb5
Brandon Weaver <a7xncob@gmail.com>
2020-02-19 20:43:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () =>
 {
-  const button = document.querySelector("form input[type=\"submit\"]");
-  button.addEventListener("click", function(event)
+  const submitButton = document.querySelector("form input[type=\"submit\"]");
+  submitButton.addEventListener("click", function(event)
   {
     event.preventDefault();
     const ul = document.getElementById("list");
@@ -12,4 +12,10 @@ document.addEventListener("DOMContentLoaded", () =>
     li.append(deleteButton);
     ul.append(li);
   });
+  
+  let buttons = document.querySelectorAll("button");
+  buttons.addEventListener("click", function(event)
+  {
+    
+  })
 });
