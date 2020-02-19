ee78f1d5519f62d1f0d3d06accf4f9b08827506f
Brandon Weaver <a7xncob@gmail.com>
2020-02-19 20:45:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ document.addEventListener("DOMContentLoaded", () =>
   let button = document.querySelector("button");
   button.addEventListener("click", function(event)
   {
-    button.parent
+    button.parentNode.remove();
   });
 });
